34c23bb537900ac3d0dc1509707294121f5b49c6
Janosch <janosch670@gmail.com>
2021-07-29 18:17:22 +0200

    Score.py added and updated warnings for scoring system

--- Score.py
+++ Score.py
@@ -1,6 +1,5 @@
 import json
 
-
 def getScore(id):
     data = __getData(id)
     room = __getRoom(data)
@@ -8,18 +7,29 @@ def getScore(id):
     print(room)
     print(sensors)
 
-    score = 6
+    warnings = []
 
-    if sensors['lightOn']:
-        score = score - 1
-    if sensors['windowsOpen'] and (sensors['airConditioningRunning'] or sensors['heaterRunning']):
-        score = score - 1
+    if sensors['lightOn'] and data['solarPowerOutput'] > 10:
+        warnings = warnings.append("Das Licht ist angeschaltet, obwohl es Hell ist")
+    if sensors['windowsOpen'] and sensors['airConditioningRunning'] :
+        warnings.append("Das Fenster ist offen, obwohl die Klimaanlage angeschlatet ist")
+    if sensors['windowsOpen'] and  sensors['heaterRunning']:
+        warnings.append("Das Fenster ist offen, obwohl die Heizung angeschlatet ist")
     if sensors['airConditioningRunning'] and sensors['heaterRunning']:
-        score = score - 1
+        warnings.append("Sowohl Heizung, als auch Klimaanlage sind eingeschaltet")
     if sensors['rollerBlindsClosed'] == False and sensors['airConditioningRunning']:
-        score = score - 1
-
-    return score
+        warnings.append("Die Klimaanlage ist an, obwohl die Rollläden oben sind")
+    if sensors['airConditioningRunning'] and room['temperature'] < 22:
+        warnings.append("Die Klimaanlage ist an, es bereits kälter als 22 Grad ist")
+    if not sensors['airConditioningRunning'] and room['temperature'] < 22:
+        if sensors['airConditioningRunning'] and room['temperature'] > data['building']['outdoorTemperature']:
+            warnings.append("Die Klimaanlage ist an, obwohl es draußen kühler als drinnen ist")
+    if sensors['airConditioningRunning'] and room['temperature'] > 24:
+        warnings.append("Die Klimaanlage ist an, es bereits kälter als 24 Grad ist")
+    if not sensors['airConditioningRunning'] and room['temperature'] < 24:
+        if sensors['airConditioningRunning'] and room['temperature'] > data['building']['outdoorTemperature']:
+            warnings.append("Die Klimaanlage ist an, obwohl es draußen kühler als drinnen ist")
+    return warnings
 
 
 def __getRoom(data):
